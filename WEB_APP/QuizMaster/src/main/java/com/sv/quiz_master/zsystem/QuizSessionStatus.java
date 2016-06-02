941ae30c902826a8a5b1c8a60365278c94a7bef0
nidura <niduraprageeth@gmail.com>
2016-06-01 22:15:09 -0700

    Master repository/service created

---
+++ WEB_APP/QuizMaster/src/main/java/com/sv/quiz_master/zsystem/QuizSessionStatus.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.sv.quiz_master.zsystem;
+
+/**
+ * Date : Jun 1, 2016
+ * Time : 8:04:51 PM
+ * @copyright : INCOSYZ
+ * @author Nidura Prageeth
+ */
+public class QuizSessionStatus {
+    public static final String PENDING = "PENDING";
+    public static final String ON_GOING = "ON_GOING";
+    public static final String COMPLETED = "COMPLETED";
+}
